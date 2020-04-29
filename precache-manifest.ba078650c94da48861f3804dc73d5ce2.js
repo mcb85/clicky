66b4e26d7ae85eeda2cf152c27ec638020edf22d
Mariana <mariana.diaz@live.com>
2020-04-29 00:34:12 -0400

    Updates

---
+++ precache-manifest.ba078650c94da48861f3804dc73d5ce2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "369627ba4747bfd80711ab23c4be40b8",
+    "url": "http://localhost:3000/index.html"
+  },
+  {
+    "revision": "70fe5a43147d4b62437c",
+    "url": "http://localhost:3000/static/css/main.e3c078fa.chunk.css"
+  },
+  {
+    "revision": "51b855b6536728e662fc",
+    "url": "http://localhost:3000/static/js/2.04a59b84.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "http://localhost:3000/static/js/2.04a59b84.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "70fe5a43147d4b62437c",
+    "url": "http://localhost:3000/static/js/main.d1b77294.chunk.js"
+  },
+  {
+    "revision": "c85b9ce77af5d0c8b896",
+    "url": "http://localhost:3000/static/js/runtime-main.8429dcf4.js"
+  }
+]);
