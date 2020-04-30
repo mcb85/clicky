adc5af3425a95f9ea7ceb5c6efe7ef601b7ad402
Mariana <mariana.diaz@live.com>
2020-04-30 03:46:35 -0400

    modified .env

--- src/App.js
+++ src/App.js
@@ -1,20 +1,15 @@
 import React from 'react';
 import './App.css';
-//import { BrowserRouter as Router, Route } from "react-router-dom"
 import NavTab from "./components/Navbar/NavTab"
 import Home from "./pages/Home"
 
 
 function App() {
   return (
-    //<Router>
       <div>
         <NavTab />
-      {/* <Route exact path = "/" component = {Home} /> */}
       <Home/>
       </div>
-
-    //</Router>
 
 
     
