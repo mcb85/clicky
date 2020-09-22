150f83ba284ccab4e9c0d4e9f34af23ea8225f32
Mariana <mariana.diaz@live.com>
2020-09-22 18:01:11 -0400

    add shuffle function, grabbing attr (hero name) when clicked an adding to picked heroesarray to state

--- src/components/HeroCard/HeroCard.js
+++ src/components/HeroCard/HeroCard.js
@@ -4,7 +4,8 @@ import "./style.css";
 export default function HeroCard(props) {
     return (
         <>
-            <img src={`/images/${props.hero}.jpg`} alt={`${props.hero}`} onClick={props.custom}/>
+            <img src={`/images/${props.hero}.jpg`} alt={`${props.hero}`} onClick={props.custom} attr={props.hero} />
+            
         </>
     )
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,4 @@
-import React, { Component }  from "react";
+import React, { Component } from "react";
 import Hero from "../components/Hero";
 import Container from "../components/Container";
 import Navbar from "../components/Navbar/NavTab";
@@ -13,36 +13,52 @@ import Col from "../components/Col/index.js";
 
 
 class Home extends Component {
-  state = {
-    heros: [
-      "black-panther", "black-widow", "capt-america", "capt-marvel", "gamora", "groot",
-      "hawkeye", "hulk", "iron-man", "scarlet-witch", "spiderman", "thor"
-    ],
-    clicked: false,
-    score: 0,
-    scoreMessage: "Score: 0 ",
-    navMessage: "Click on an image to begin!",
-  };
-  
+  constructor(props) {
+    super(props);
+    this.state = {
+      heros: [
+        "black-panther", "black-widow", "capt-america", "capt-marvel", "gamora", "groot",
+        "hawkeye", "hulk", "iron-man", "scarlet-witch", "spiderman", "thor"
+      ],
+      pickedHeros: [],
+      //clicked: false,
+      score: 0,
+      scoreMessage: "Score: 0 ",
+      navMessage: "Click on an image to begin!",
+    };
+
+    this.handleClick = this.handleClick.bind(this);
+  }
 
   shuffleArray = (heros) => {
     for (let i = this.state.heros.length - 1; i > 0; i--) {
       const j = Math.floor(Math.random() * (i + 1));
       [this.state.heros[i], this.state.heros[j]] = [this.state.heros[j], this.state.heros[i]];
     }
-  
-}
 
-  handleClick = () => {
-    console.log("CLICKED " + this.state.hero)
+  }
+
+
+
+  //checkMatch = (e.currentTarget.getAttribute('attr'))) => {
+
+  //}
+
+  handleClick = (e) => {
+    let imageClicked = (e.currentTarget.getAttribute('attr'))
+    console.log("CLICKED  " + imageClicked)
+
+
+
     this.setState({
-      clicked: !this.state.clicked,
+      //clicked: !this.state.clicked,
       score: this.state.score + 1,
       scoreMessage: "Score: " + (this.state.score + 1),
-      navMessage: "You guessed correctly!"
+      navMessage: "You guessed correctly!",
+      pickedHeros: this.state.pickedHeros + imageClicked
     });
-   
     this.shuffleArray();
+    console.log(this.state.pickedHeros);
   };
 
   handleAlreadyClicked = () => {
@@ -53,32 +69,27 @@ class Home extends Component {
       navMessage: "Try again!"
     });
     this.shuffleArray();
-}
+  }
 
   render() {
     return (
       <div>
-        <Navbar scoreMessage={this.state.scoreMessage} navMessage={this.state.navMessage}/>
+        <Navbar scoreMessage={this.state.scoreMessage} navMessage={this.state.navMessage} />
         <Hero backgroundImage="./images/double-bubble-dark.png">
           <h1>Clicky Game</h1>
           <h3 className="text-white">Click on an image to earn points. Click on each image only once!</h3>
         </Hero>
-    
+
         <Container>
           <Row>
-            
-          
+
+
             {this.state.heros.map(hero => {
               return (
                 <Col size="sm-4" key={hero}>
                   <div className="card">
-                    {/* {this.state.clicked ===false ? ( */}
-                      <HeroCard hero={hero} custom={this.handleClick} />
-                    {/* ) */}
-                      {/* : ( */}
-                    {/* <HeroCard hero={hero} custom={this.handleAlreadyClicked} /> */}
-                        {/* ) */}
-                    {/* } */}
+                    <HeroCard hero={hero} custom={this.handleClick}
+                    />
                   </div>
                 </Col>
               );
@@ -90,6 +101,6 @@ class Home extends Component {
       </div>
     );
   }
-  }
+}
 
 export default Home;
